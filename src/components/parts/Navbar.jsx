8720304c93e19a0487b190afb39233fa36584952
aryadira <aryadyra27@gmail.com>
2023-10-09 13:50:57 +0700

    50% navbar

--- src/components/parts/Navbar.jsx
+++ src/components/parts/Navbar.jsx
@@ -1,30 +1,181 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { BoxContainer, SpacingLayout } from "../../layouts";
+import { consul } from "../../assets/img";
 import "./styles/navbar.css";
+// import "./js/navbar";
+
+const NavOverlayDown = ({ active, pages }) => {
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
+  const isLink = (pages) => {
+    if (pages == "Products") {
+      return (
+        <ul className='w-[20%] text-base'>
+          <li onClick={refreshPage}>
+            <Link to={"/products"} className='block mb-4'>
+              Discover All
+            </Link>
+            <Link to={"/products/seeds"} className='block'>
+              Seeds
+            </Link>
+            <Link to={"/products/materials"} className='block'>
+              Materials
+            </Link>
+            <Link to={"/products/tools"} className='block'>
+              Tools
+            </Link>
+            <Link to={"/products/fertilizers"} className='block'>
+              Fertilizer
+            </Link>
+          </li>
+        </ul>
+      );
+    } else if (pages == "Services") {
+      return (
+        <ul className='w-[20%] text-base'>
+          <li onClick={refreshPage}>
+            <Link to={"/services"} className='block mb-4'>
+              Discover All
+            </Link>
+            <Link to={"/services/green-infrastructure"} className='block'>
+              Green infrastructure
+            </Link>
+            <Link to={"/services/smart-irrigation"} className='block'>
+              Smart irrigation
+            </Link>
+            <Link to={"/services/agriculture-plantation"} className='block'>
+              Agriculture plantation
+            </Link>
+            <Link to={"/services/vertical-farming"} className='block'>
+              Vertical farming
+            </Link>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className='lg:w-[16%] w-10% text-base '>
+          <li className='mb-5' onClick={refreshPage}>
+            <Link to={"/products"} className='block'>
+              Products
+            </Link>
+            <Link to={"/products/seeds"} className='block'>
+              Seeds
+            </Link>
+            <Link to={"/products/materials"} className='block'>
+              Materials
+            </Link>
+            <Link to={"/products/tools"} className='block'>
+              Tools
+            </Link>
+            <Link to={"/products/fertilizers"} className='block'>
+              Fertilizer
+            </Link>
+          </li>
+          <li onClick={refreshPage}>
+            <Link to={"/services"} className='block'>
+              Services
+            </Link>
+            <Link to={"/services/green-infrastructure"} className='block'>
+              Green infrastructure
+            </Link>
+            <Link to={"/services/smart-irrigation"} className='block'>
+              Smart irrigation
+            </Link>
+            <Link to={"/services/agriculture-plantation"} className='block'>
+              Agriculture plantation
+            </Link>
+            <Link to={"/services/vertical-farming"} className='block'>
+              Vertical farming
+            </Link>
+          </li>
+          <div className='lg:flex text-base hidden items-center'>
+            <Link to={"/signin"} className='mr-6'>
+              Sign in
+            </Link>
+            <Link to={"/signup"} className='bg-black text-white px-8 py-3 rounded-full'>
+              Join Us
+            </Link>
+          </div>
+        </ul>
+      );
+    }
+  };
+
+  return (
+    <div
+      className={`clicked-link w-full bg-white shadow-md backdrop-blur-sm bg-opacity-80 absolute transition duration-500 ease-in-out delay-100 py-5 -z-10 ${
+        active == "" ? "-translate-y-[600px]" : ""
+      }`}>
+      <SpacingLayout>
+        <BoxContainer>
+          <nav className='navbar lg:flex block  justify-between py-3'>
+            <div className='link-title pr-[100px]  mb-2'>
+              <h2 className='font-HaasBd text-sm'>{pages}</h2>
+            </div>
+            {isLink(pages)}
+            <div>
+              <img src={consul} alt='' className='w-[280px] hidden md:block' />
+            </div>
+          </nav>
+        </BoxContainer>
+      </SpacingLayout>
+    </div>
+  );
+};
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+  const [isPage, setIsPage] = useState("");
+
+  const overlayDown = (pages) => {
+    setIsActive(!isActive);
+    setIsPage(pages);
+  };
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
-    <header className='w-full block bg-red-100'>
+    <header className='w-full bg-white top-0 fixed'>
       <SpacingLayout>
         <BoxContainer>
           <nav className='navbar flex justify-between py-3 items-center'>
-            <div className='logo'>
-              <h2 className='font-HaasBd text-2xl'>Greenverse</h2>
+            <div className='logo' onClick={refreshPage}>
+              <Link to={"/"} className='font-HaasBd text-2xl'>
+                Greenverse
+              </Link>
             </div>
 
             {/* desktop links */}
             <ul className='lg:flex text-base hidden'>
               <li className='px-2'>
-                <Link to={"/products"}>Products</Link>
+                <div
+                  onClick={() => {
+                    overlayDown("Products");
+                  }}
+                  className='cursor-pointer'>
+                  Products
+                </div>
               </li>
               <li className='px-2'>
-                <Link to={"/services"}>Services</Link>
+                <div
+                  onClick={() => {
+                    overlayDown("Services");
+                  }}
+                  className='cursor-pointer'>
+                  Services
+                </div>
               </li>
               <li className='px-2'>
-                <Link to={"/topics"}>Topics</Link>
+                <div className='cursor-pointer'>Topics</div>
               </li>
               <li className='px-2'>
-                <Link to={"/consultation"}>Consultation</Link>
+                <div className='cursor-pointer'>Consultation</div>
               </li>
             </ul>
             <div className='lg:flex text-base hidden items-center'>
@@ -37,79 +188,20 @@ const Navbar = () => {
             </div>
 
             {/* mobile links */}
-            <ul className='mobile-nav hidden w-full '>
+            <ul className='mobile-nav hidden w-full'>
               <li>
-                <div className='menu-icon-container '>
+                <div className='menu-icon-container' onClick={() => overlayDown("")}>
                   <div className='menu-icon'>
                     <span className='line-1'></span>
                     <span className='line-2'></span>
                   </div>
                 </div>
               </li>
-
-              <li>
-                <a href='#' className='link-logo'></a>
-              </li>
-
-              <li>
-                <a href='' className='link-bag'></a>
-              </li>
             </ul>
           </nav>
         </BoxContainer>
       </SpacingLayout>
-
-      {/* clicked links */}
-      <div className='clicked-link bg-red-500 py-5'>
-        <SpacingLayout>
-          <BoxContainer>
-            <nav className='navbar flex justify-between py-3 items-center'>
-              <div className='link-title'>
-                <h2 className='font-HaasBd text-sm'>Products</h2>
-              </div>
-              <ul className='text-base bg-blue-200  w-full'>
-                <li>
-                  <Link to={"/products"} className='block'>
-                    Products
-                  </Link>
-                  <Link to={"/products/seeds"} className='block'>
-                    Seeds
-                  </Link>
-                  <Link to={"/products/materials"} className='block'>
-                    Materials
-                  </Link>
-                  <Link to={"/products/tools"} className='block'>
-                    Tools
-                  </Link>
-                  <Link to={"/products/fertilizers"} className='block'>
-                    Fertilizer
-                  </Link>
-                </li>
-                <li className='bg-green-500 '>
-                  <Link to={"/services"} className='block'>
-                    Services
-                  </Link>
-                  <Link to={"/services/green-infrastructure"} className='block'>
-                    Green infrastructure
-                  </Link>
-                  <Link to={"/services/smart-irrigation"} className='block'>
-                    Smart irrigation
-                  </Link>
-                  <Link to={"/services/agriculture-plantation"} className='block'>
-                    Agriculture plantation
-                  </Link>
-                  <Link to={"/services/vertical-farming"} className='block'>
-                    Vertical farming
-                  </Link>
-                </li>
-              </ul>
-              <div>
-                <img src='' alt='' />
-              </div>
-            </nav>
-          </BoxContainer>
-        </SpacingLayout>
-      </div>
+      <NavOverlayDown active={isActive} pages={isPage} />
     </header>
   );
 };
