fb59d8261567f9babf774053935657c44e5b5710
aryadira <aryadyra27@gmail.com>
2023-10-09 08:16:02 +0700

    add navbar dummy

--- src/components/parts/Navbar.jsx
+++ src/components/parts/Navbar.jsx
@@ -6,34 +6,37 @@ const Navbar = () => {
     <header>
       <SpacingLayout>
         <BoxContainer>
-          <nav>
-            <ul>
-              <li>
+          <nav className='flex justify-between py-3 items-center'>
+            <div className='logo'>
+              <h2>Greenverse</h2>
+            </div>
+            <ul className='flex font-medium'>
+              <li className='px-2'>
                 <Link to={"/"}>Home</Link>
               </li>
               <div>
-                <li>
+                <li className='px-2'>
                   <Link to={"/products"}>Products</Link>
-                  <Link to={"/products/seeds"}>Seeds</Link>
-                  <Link to={"/products/materials"}>Materials</Link>
-                  <Link to={"/products/tools"}>Tools</Link>
-                  <Link to={"/products/fertilizers"}>Fertilizer</Link>
                 </li>
               </div>
-              <li>
+              <li className='px-2'>
                 <Link to={"/services"}>Services</Link>
-                <Link to={"/services/green-infrastructure"}>Green infrastructure</Link>
-                <Link to={"/services/smart-irrigation"}>Smart irrigation</Link>
-                <Link to={"/services/agriculture-plantation"}>Agriculture plantation</Link>
-                <Link to={"/services/vertical-farming"}>Vertical farming</Link>
               </li>
-              <li>
+              <li className='px-2'>
                 <Link to={"/topics"}>Topics</Link>
               </li>
-              <li>
+              <li className='px-2'>
                 <Link to={"/consultation"}>Consultation</Link>
               </li>
             </ul>
+            <div className='flex font-medium items-center'>
+              <a href='#' className='mr-6'>
+                Sign Up
+              </a>
+              <a href='#' className='bg-black text-white px-8 py-3 rounded-full'>
+                Join Us
+              </a>
+            </div>
           </nav>
         </BoxContainer>
       </SpacingLayout>
--- src/dummy/NavbarDummy.jsx
+++ src/dummy/NavbarDummy.jsx
@@ -2,31 +2,39 @@ import React from "react";
 
 const NavbarDummy = () => {
   return (
-    <nav className="flex justify-between h-[74px] items-center px-24">
-      <div>
-        <img src={logo} alt="" />
+    <nav className='flex justify-between h-[74px] items-center px-24'>
+      <div className='logo'>
+        <h2>Greenverse</h2>
       </div>
 
-      <ul className="flex font-medium">
-        <li className="px-2">
-          <a href="#">Products</a>
+      <ul className='flex font-medium'>
+        <li className='px-2'>
+          <a href='#'>Products</a>
+          <Link to={"/products/seeds"}>Seeds</Link>
+          <Link to={"/products/materials"}>Materials</Link>
+          <Link to={"/products/tools"}>Tools</Link>
+          <Link to={"/products/fertilizers"}>Fertilizer</Link>
         </li>
-        <li className="px-2">
-          <a href="#">Services</a>
+        <li className='px-2'>
+          <a href='#'>Services</a>
+          <Link to={"/services/green-infrastructure"}>Green infrastructure</Link>
+          <Link to={"/services/smart-irrigation"}>Smart irrigation</Link>
+          <Link to={"/services/agriculture-plantation"}>Agriculture plantation</Link>
+          <Link to={"/services/vertical-farming"}>Vertical farming</Link>
         </li>
-        <li className="px-2">
-          <a href="#">Topics</a>
+        <li className='px-2'>
+          <a href='#'>Topics</a>
         </li>
-        <li className="px-2">
-          <a href="#">Consultations</a>
+        <li className='px-2'>
+          <a href='#'>Consultations</a>
         </li>
       </ul>
 
-      <div className="flex font-medium items-center">
-        <a href="#" className="mr-6">
+      <div className='flex font-medium items-center'>
+        <a href='#' className='mr-6'>
           Sign Up
         </a>
-        <a href="#" className="bg-black text-white px-8 py-3 rounded-full">
+        <a href='#' className='bg-black text-white px-8 py-3 rounded-full'>
           Join Us
         </a>
       </div>
