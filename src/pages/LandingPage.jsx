d7b53fc06022520851a6178cc19e2518b1c3e13c
Aven1426 <fathir.adlan@gmail.com>
2023-10-09 08:11:42 +0700

    dummy, svg assets added

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,24 +2,24 @@ import { BoxContainer, SpacingLayout } from "../layouts";
 
 const LandingPage = () => {
   return (
-    <div className='landing-page'>
+    <div className="landing-page">
       <SpacingLayout>
         <BoxContainer>
-          <div className='box-1 bg-red-300'>BOX 1</div>
-          <div className='box-2 bg-red-400'>BOX 2</div>
+          <div className="box-1 bg-red-300">BOX 1</div>
+          <div className="box-2 bg-red-400">BOX 2</div>
         </BoxContainer>
       </SpacingLayout>
-      <div className='box-3 bg-red-500'>
-        <SpacingLayout className='bg-green-300'>
+      <div className="box-3 bg-red-500">
+        <SpacingLayout className="bg-green-300">
           <BoxContainer>BOX 3</BoxContainer>
         </SpacingLayout>
       </div>
       <SpacingLayout>
         <BoxContainer>
-          <div className='box-4 bg-red-600'>BOX 4</div>
+          <div className="box-4 bg-red-600">BOX 4</div>
         </BoxContainer>
       </SpacingLayout>
-      <div className='box-5 bg-red-700'>
+      <div className="box-5 bg-red-700">
         <SpacingLayout>
           <BoxContainer>BOX 5</BoxContainer>
         </SpacingLayout>
