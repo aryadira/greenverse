6ca9992be453bc80a21e3c9cff25754caf2696ea
Aven1426 <fathir.adlan@gmail.com>
2023-10-13 17:07:54 +0700

    constant last farming

--- src/components/sections/ProductSection.jsx
+++ src/components/sections/ProductSection.jsx
@@ -5,17 +5,21 @@ import { crimson } from "../../assets/img";
 
 const ProductSection = () => {
   return (
-    <div className='border-t-2 pt-[90px] border-[#0000004d]'>
-      <h1 className='text-[32px] font-HaasMd'>Seeds Type List</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+    <div className="border-t-2 pt-[90px] border-[#0000004d]">
+      <h1 className="text-[32px] font-HaasMd">Seeds Type List</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         {displayAll.map((content) => (
-          <div className='card-content mt-8 rounded-[30px] hover:rounded-none'>
-            <div className=' relative overflow-hidden bg-cover bg-no-repeat '>
-              <img src={content.img} alt='' className='w-full transition duration-300 ease-in-out hover:scale-110 cursor-pointer' />
+          <div className="card-content mt-8 rounded-[30px]">
+            <div className=" relative overflow-hidden bg-cover bg-no-repeat ">
+              <img
+                src={content.img}
+                alt=""
+                className="w-full transition duration-300 ease-in-out hover:scale-110 hover:rounded-[30px]  cursor-pointer"
+              />
             </div>
-            <div className='text pt-4 bg-white cursor-pointer'>
-              <h1 className='text-sm text-[#676767]'>{content.type}</h1>
-              <h1 className='text-2xl font-HaasMd'>{content.title}</h1>
+            <div className="text pt-4 bg-white cursor-pointer">
+              <h1 className="text-sm text-[#676767]">{content.type}</h1>
+              <h1 className="text-2xl font-HaasMd">{content.title}</h1>
             </div>
           </div>
         ))}
