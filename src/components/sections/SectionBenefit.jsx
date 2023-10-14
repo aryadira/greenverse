8bb0cc5f1ff4edfc4a0c0ce2c73ef6161ddbe408
Adiptra <adiirawan2705@gmail.com>
2023-10-14 16:22:56 +0700

    animasi secion benefit

--- src/components/sections/SectionBenefit.jsx
+++ src/components/sections/SectionBenefit.jsx
@@ -12,19 +12,21 @@ const sectionBenefit = () => {
     const tl = gsap.timeline(
       {
         scrollTrigger: {
-          trigger: ".left-img",
-          start: "top 80%",
-          end: "top 30%",
+          trigger: ".container-content",
+          start: "top 100%",
+          end: "top 40%",
           scrub: 5,
+          markers: true,
         },
       },
       []
     );
 
-    tl.set(".left-img", { xPercent: 200, opacity: 0 })
-      .to(".left-img", { y: 10, duration: 2 })
-      .to(".left-img", { xPercent: 0, duration: 2, opacity: 100 })
-      .to(".left-img", { y: 0, duration: 4 });
+    tl.from(".left-img", { xPercent: -100, yPercent: 150 }).to(".left-img", {
+      xPercent: 0,
+      yPercent: 0,
+      duration: 2,
+    });
 
     // SplitText Animate
     let textToSplit = new SplitText(".title", { types: "chars" });
@@ -46,25 +48,29 @@ const sectionBenefit = () => {
     });
   }, []);
   return (
-    <div className='wrapper mt-[120px]'>
-      <div className='text-center'>
-        <p className='title text-[#676767] text-xl font-base'>Reasons.</p>
-        <h1 className='title text-[32px] mb-8 font-HaasMd'>Why should choose Greenverse?</h1>
+    <div className="wrapper mt-[120px]">
+      <div className="text-center">
+        <p className="title text-[#676767] text-xl font-base">Reasons.</p>
+        <h1 className="title text-[32px] mb-8 font-HaasMd">
+          Why should choose Greenverse?
+        </h1>
       </div>
-      <div className='flex justify-between items-start flex-wrap lg:flex-nowrap gap-3'>
-        <div className='left-img bg-white w-full lg:w-1/2 flex justify-center rounded-[30px] mr-1 mb-3'>
-          <img src={landingImg2} alt='' className='w-[70%] px-6 pt-6' />
+      <div className="container-content flex justify-between items-start flex-wrap lg:flex-nowrap gap-3">
+        <div className="left-img bg-white w-full lg:w-1/2 flex justify-center rounded-[30px] mr-1 mb-3">
+          <img src={landingImg2} alt="" className="w-[70%] px-6 pt-6" />
         </div>
-        <div className='w-full lg:w-1/2 '>
-          <div className='right-benefit gap-3 grid grid-cols-2'>
+        <div className="w-full lg:w-1/2 ">
+          <div className="right-benefit gap-3 grid grid-cols-2">
             {benefits.map((benefit) => (
               // eslint-disable-next-line react/jsx-key
-              <div className='card pt-[20px] px-[10px] pb-[50px] bg-white rounded-[30px]'>
-                <p className='my-[10px] mx-[10px] bg-gradient-to-r from-[#50FF96] to-white inline py-[4px] pl-[12px] pr-5 rounded-full'>
+              <div className="card pt-[20px] px-[10px] pb-[50px] bg-white rounded-[30px]">
+                <p className="my-[10px] mx-[10px] bg-gradient-to-r from-[#50FF96] to-white inline py-[4px] pl-[12px] pr-5 rounded-full">
                   {benefit.id}
                 </p>
-                <h3 className='title-heading my-[10px] mx-[10px] text-2xl  font-HaasMd'>{benefit.title}</h3>
-                <p className='my-[10px] mx-[10px]'>{benefit.desc} </p>
+                <h3 className="title-heading my-[10px] mx-[10px] text-2xl  font-HaasMd">
+                  {benefit.title}
+                </h3>
+                <p className="my-[10px] mx-[10px]">{benefit.desc} </p>
               </div>
             ))}
           </div>
