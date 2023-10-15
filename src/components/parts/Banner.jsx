564e9cda32a5448c48a4ae27ac248b02aae642ff
Adiptra <adiirawan2705@gmail.com>
2023-10-16 00:21:46 +0700

    Animasi Parallax

--- src/components/parts/Banner.jsx
+++ src/components/parts/Banner.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import { useEffect } from "react";
 import { servicesBanner } from "../../assets/img";
 import { BoxContainer, SpacingLayout } from "../../layouts";
+import { gsap } from "gsap-trial";
+import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
+import { SplitText } from "gsap-trial/SplitText";
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const Banner = ({ title, children, image, subtitle }) => {
+  useEffect(() => {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".textParallax",
+        start: "top 20%",
+        end: "bottom 20%",
+        scrub: 1,
+      },
+    });
+
+    tl.from(".textParallax", { yPercent: 0 }).to(".textParallax", {
+      yPercent: 100,
+      ease: "back",
+    });
+  });
   return (
     <div>
-      <div className='w-full banner mt-7 relative'>
-        <img src={image ? image : servicesBanner} className='w-full h-[50vh] object-cover' alt='' />
+      <div className="w-full banner mt-7 relative">
+        <img
+          src={image ? image : servicesBanner}
+          className="w-full h-[50vh] object-cover"
+          alt=""
+        />
         <SpacingLayout>
-          <div className='w-full text absolute bottom-1/2 translate-y-1/2 text-xl left-0 sm:left-[100px] lg:left-[120px] text-center sm:text-left'>
-            <p className='text-[#C8C8C8] sm:text-xl text-base'>{subtitle}</p>
-            <p className='text-[#C8C8C8] text-xl lg:text-[40px] leading-7 lg:leading-[44px]'>
-              <span className='text-white'>{title} </span>
+          <div className="textParallax w-full text absolute bottom-1/2 translate-y-1/2 text-xl left-0 sm:left-[100px] lg:left-[120px] text-center sm:text-left">
+            <p className="text-[#C8C8C8] sm:text-xl text-base">{subtitle}</p>
+            <p className="text-[#C8C8C8] text-xl lg:text-[40px] leading-7 lg:leading-[44px]">
+              <span className="text-white">{title} </span>
               {children}
             </p>
           </div>
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,4 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Banner from "../../components/parts/Banner";
 import { BoxContainer, SpacingLayout } from "../../layouts";
 import ProductSection from "../../components/sections/ProductSection";
@@ -16,46 +17,48 @@ const Products = () => {
   const withoutStrip = finalPath.replace(/[._-]/g, " ");
 
   return path.pathname != "/products" ? (
-    <div className='products'>
+    <div className="products">
       <Helmet>
-        <title>{path.pathname.match("-") ? withoutStrip : finalPath} | Greenverse</title>
+        <title>
+          {path.pathname.match("-") ? withoutStrip : finalPath} | Greenverse
+        </title>
       </Helmet>
       <Outlet />
     </div>
   ) : (
-    <div className='wrapper'>
+    <div className="wrapper">
       <Helmet>
         <title>Products | Greenverse</title>
       </Helmet>
-      <div className='container-wrapper'>
-        <Banner title='Products.' subtitle='Discover All'>
+      <div className="container-wrapper">
+        <Banner title="Products." subtitle="Discover All">
           Explore <br />
           our products to get some benefits
         </Banner>
-        <SpacingLayout className=''>
+        <SpacingLayout className="">
           <BoxContainer>
-            <div className='w-full types grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 gap-6'>
-              <div className='bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform'>
+            <div className="w-full types grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+              <div className="bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform">
                 <h1>brassica</h1>
               </div>
-              <div className='bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform'>
+              <div className="bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform">
                 <h1>Cover Crop Mixes</h1>
               </div>
-              <div className='bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform'>
+              <div className="bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform">
                 <h1>Grasses, Millets, Sorghums</h1>
               </div>
-              <div className='bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform'>
+              <div className="bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform">
                 <h1>Grains</h1>
               </div>
-              <div className='bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform'>
+              <div className="bg-white hover:bg-neutral-200 shadow-sm flex justify-center items-center h-44 text-lg p-4 text-center rounded-2xl my-6 hover:scale-110 transition-transform">
                 <h1>Legumes</h1>
               </div>
             </div>
           </BoxContainer>
         </SpacingLayout>
-        <Marquee className='h-[0px]'>Let's Find Our Products!</Marquee>
+        <Marquee className="h-[0px]">Let's Find Our Products!</Marquee>
         <SpacingLayout>
-          <BoxContainer className='mt-[100px]'>
+          <BoxContainer className="mt-[100px]">
             <ProductSection />
           </BoxContainer>
         </SpacingLayout>
